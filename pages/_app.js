733f5f9727ad7d724a693977a5a260afc4ac1163
BayHatter <bayhatter@gmail.com>
2025-05-01 17:54:19 +0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,5 @@
 import "@/styles/globals.css";
-import { NextIntlClientProvider } from 'next-intl';
-import { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps }) {
-  const router = useRouter();
-  const { locale, defaultLocale } = router;
-
-  const messages = pageProps.messages || {};
-
-  return (
-    <>
-      <NextIntlClientProvider locale={locale} messages={messages}>
-        <Component {...pageProps} />
-      </NextIntlClientProvider>
-    </>
-  );
+  return <Component {...pageProps} />;
 }
